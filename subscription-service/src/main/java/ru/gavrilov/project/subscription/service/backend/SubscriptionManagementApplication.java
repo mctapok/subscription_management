2432082c62ffad32f697c79e1691e0e0aa0a906c
mctapok <vektor.ct@gmail.com>
2024-09-08 11:54:31 +0300

    git init

---
+++ subscription-service/src/main/java/ru/gavrilov/project/subscription/service/backend/SubscriptionManagementApplication.java
@@ -0,0 +1,13 @@
+package ru.gavrilov.project.subscription.service.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionManagementApplication.class, args);
+	}
+
+}
---
+++ subscription-service/src/test/java/ru/gavrilov/project/subscription/service/backend/SubscriptionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gavrilov.project.subscription.service.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
