c609c36e0fb0acae79fe1b9a4a6f180face48421
mctapok <vektor.ct@gmail.com>
2026-01-12 23:28:52 +0300

    feat: добавить flyway, refactor: изменить архитектуру бд
    добавил flyway
    откорректировал имена и расположение .sql сриптов
    обновил application.yaml сервисов
    обновил docker-compose.yml в соответствии с архитектурой бд, разделил бд на каждый сервис
    удалил ненужные импорты в GlobalExceptionHandler

--- subscription-service/src/main/resources/db/migration/V1__init.sql
+++ subscription-service/src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,13 @@
+create table subscription_plans
+(
+    id                 bigserial primary key,
+    subscription_title varchar(255),
+    duration           bigint,
+    price              numeric(6, 2),
+    created_at         timestamp,
+    updated_at         timestamp
+);
+
 create table subscriptions
 (
     id                 bigserial primary key,
@@ -12,23 +22,15 @@ create table subscriptions
     foreign key (subscription_id) references subscription_plans(id) on delete cascade
 );
 
-create table subscription_plans
-(
-    id                 bigserial primary key,
-    subscription_title varchar(255),
-    duration           bigint,
-    price              numeric(6, 2),
-    created_at         timestamp,
-    updated_at         timestamp
-);
-
-insert into subscriptions (subscription_id, client_id, subscription_title, duration, price, status)
-VALUES (1, 1, 'netflix', 30, 15, 'active'),
-       (5, 1, 'photoshop', 30, 9, 'active');
-
 insert into subscription_plans (subscription_title, duration, price)
 VALUES ('netflix', 30, 13.99),
        ('spotify', 30, 9.99),
        ('amazon prime', 30, 14.95),
        ('bank BOA premium', 30, 4.95),
        ('photoshop', 30, 17.95);
+
+insert into subscriptions (subscription_id, client_id, subscription_title, duration, price, status)
+VALUES (1, 1, 'netflix', 30, 15, 'active'),
+       (5, 1, 'photoshop', 30, 9, 'active');
+
+
